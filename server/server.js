03f61c1f38802d3e69289800a55c398e2fe109ec
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2019-05-15 12:07:27 -0700

    nginx serves static index.  express routes request for static content to nginx. LOCALLY

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
 // app.get('/', (req, res) => res.send('yo doggy'));
-app.use('/', express.static(path.join(__dirname, '../client/public')));
+// app.use('/', express.static(path.join(__dirname, '../client/public')));
+
+app.get('/', (req, res) => {
+  axios
+    .get('http://localhost:8080/index.html')
+    .then(response => res.send(response.data));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
